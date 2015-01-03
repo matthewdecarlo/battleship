2aec7d4109be9a5975abaa02a96ae834cac4d94b
Matthew DeCarlo <matthewwho@me.com>
2015-01-03 17:58:48 -0600

    started adding ships

--- source/battleship.rb
+++ source/battleship.rb
@@ -1,17 +1,17 @@
 Logo = %q"
-  🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓
-⚓  _  _  _  _   __     ____  _  _  __ _  __ _    _  _  _  _            🌊️            
-🌊 / )( \/ )( \ /  \   / ___)/ )( \(  ( \(  / )  ( \/ )( \/ )           ⚓️
-⚓ \ /\ /) __ ((  O )  \___ \) \/ (/    / )  (   / \/ \ )  /_  _  _     🌊️
-🌊 (_/\_)\_)(_/ \__/   (____/\____/\_)__)(__\_)  \_)(_/(__/(_)(_)(_)    ⚓
-⚓ ________       ________________           ______ _____        ______ 🌊️
-🌊 ___  __ )_____ __  /__  /___  /______________  /____(_)__________  / ⚓
-⚓ __  __  |  __ `/  __/  __/_  /_  _ \_  ___/_  __ \_  /___  __ \_  /  🌊
-🌊 _  /_/ // /_/ // /_ / /_ _  / /  __/(__  )_  / / /  / __  /_/ //_/   ⚓
-⚓ /_____/ \__,_/ \__/ \__/ /_/  \___//____/ /_/ /_//_/  _  .___/(_)    🌊
-🌊                                                      /_/             ⚓
-⚓ a simple game by matthewwho                                          🌊
-  🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓️
+  🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓ ️🌊 ⚓️
+⚓  _  _  _  _   __     ____  _  _  __ _  __ _    _  _  _  _           🌊️            
+🌊 / )( \/ )( \ /  \   / ___)/ )( \(  ( \(  / )  ( \/ )( \/ )          ⚓️
+⚓ \ /\ /) __ ((  O )  \___ \) \/ (/    / )  (   / \/ \ )  /_  _  _    🌊️
+🌊 (_/\_)\_)(_/ \__/   (____/\____/\_)__)(__\_)  \_)(_/(__/(_)(_)(_)   ⚓
+⚓ ________       ________________           ______ _____        ______🌊️
+🌊 ___  __ )_____ __  /__  /___  /______________  /____(_)__________  /⚓
+⚓ __  __  |  __ `/  __/  __/_  /_  _ \_  ___/_  __ \_  /___  __ \_  / 🌊
+🌊 _  /_/ // /_/ // /_ / /_ _  / /  __/(__  )_  / / /  / __  /_/ //_/  ⚓
+⚓ /_____/ \__,_/ \__/ \__/ /_/  \___//____/ /_/ /_//_/  _  .___/(_)   🌊
+🌊                                                      /_/            ⚓
+⚓ a simple game by matthewwho                                         🌊
+  🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓️ 🌊 ⚓ ️🌊 ⚓ ️🌊 ⚓️
 "
 
 class Board
@@ -19,38 +19,60 @@ class Board
 
   def initialize()
     @board = {}
-
+    ships = [5, 4, 3, 2, 2, 2, 1, 1, 1]
     gen_board
   end
 
   def gen_board
     ("A".."J").each do |row|
-      (1..10).each { |column| board.merge!( { "#{row}#{column}".to_sym => '🌊' } ) }
+      (1..10).each { |column| board.merge!( { "#{row}#{column}".to_sym => '1' } ) }
     end
 
     return board
   end
 
   def board_array_representation
-    board_string = []
+    final_board_array = []
     board_array = []
     letter = "A"
 
     board.each_value { |value| board_array << value }
-    board_string << "    1  2  3  4  5  6  7  8  9  10  "
-    board_string << "  +-------------------------------+"
+    final_board_array << "    1 2 3 4 5 6 7 8 9 10"
+    final_board_array << "  +---------------------+"
 
     board_array.each_slice(10) do |row|
-      board_string << "#{letter} | " + row.join(" ").gsub(/["0"]/, '_') + " |"
+      final_board_array << "#{letter} | " + row.join(" ").gsub(/["0"]/, '🌊') + " |"
       letter.next!
     end
 
-    board_string << "  +-------------------------------+"
+    final_board_array << "  +---------------------+"
 
-    return board_string
+    return final_board_array
+  end
+
+  def get_random_position
+    self.board.keys.sample
+  end
+
+  def has_ship?(a_value)
+    a_value == '1'
+  end
+
+  def place_ship_horizontaly
+    get_random_position
+  end
+
+end
+
+class Player
+  # attr_accessor
+
+  def initialize
   end
 end
 
+
+## Runner
 def display_game(first_board, second_board)
   display = ""
   
@@ -59,9 +81,14 @@ def display_game(first_board, second_board)
   return display
 end
 
+
+
+##TESTS
 player_a = Board.new
 player_b = Board.new
 
 puts Logo
 puts
 puts display_game(player_a.board_array_representation, player_b.board_array_representation)
+
+print player_a.horizontal_ship_placement
